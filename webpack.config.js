ac13e0dc956d12d1df219dada822ccd94d2ef9b1
Tatsuya Sonobe <tatsuya_sonobe@jp.honda>
2022-10-21 17:25:24 +0900

    [add] webpack & build src/index.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "src/index.js",
+  output: {
+    path: __dirname + "/src/build_js",
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: "babel-loader",
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2021 を ES5 に変換
+                "@babel/preset-env",
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // ES5(IE11等)向けの指定
+  target: ["web", "es5"],
+};
